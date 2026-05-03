f14baff0f6f8d38ec2783530c2011144116233df
novalCandra <dinatanovel5@gmail.com>
2026-05-03 14:11:08 +0700

    feat : ✅ Done API Auth

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,48 +1,22 @@
-import { PrismaClient } from "../generated/prisma/index.js";
-import bcrypt from "bcrypt";
-import { response } from "express";
-import jwt from "jsonwebtoken"
-const prisma = new PrismaClient();
+import { LoginSevices } from "../services/auth.service.js";
 export const ControllerLogin = async (req, res) => {
     try {
         const { email, password } = req.body;
-        const admin = await prisma.admin.findUnique({
-            where: { email }
-        })
+        const { admin, token } = await LoginSevices({ email, password })
 
-        if (!admin) {
-            return res.status(403).json({
-                status: false,
-                message: "email and passord no database"
-            })
-        }
-
-        const isValid = await bcrypt.compare(password, admin.password);
-        if (!isValid) {
-            return res.status(401).json({
-                status: false,
-                message: "Wrong Password"
-            })
-        }
-        const { password: _, ...safeAdmin } = admin;
-
-        const token = jwt.sign(
-            { id: admin?.id, email: admin?.email, },
-            process.env.SECRET_KEY_JWT,
-            { expiresIn: "1d" }
-        )
-        return res.status(201).json({
+        return res.status(200).json({
             status: true,
-            message: "Login Success",
+            message: "Login success",
             data: admin,
-            token: token
-        })
+            token
+        });
+
     } catch (error) {
-        console.error(error)
-        return res.status(500).json({
+        const statusCode = error.statusCode ?? 500;
+        return res.status(statusCode).json({
             status: false,
-            message: "Internal Error Server"
-        })
+            message: error.message || "Internal Server Error"
+        });
     }
 }
 
---
+++ services/auth.service.js
@@ -0,0 +1,32 @@
+import prisma from "../lib/Prisma.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
+export const LoginSevices = async ({ email, password }) => {
+
+    const admin = await prisma.admin.findUnique({
+        where: { email }
+    });
+
+    if (!admin) {
+        const error = new Error("Email not Found");
+        error.statusCode = 403;
+        throw error
+    };
+
+    const isValid = await bcrypt.compare(password, admin.password);
+    if (!isValid) {
+        const error = new Error("Wrong password");
+        error.statusCode = 401
+        throw error;
+    }
+
+    const { password: _, ...safeAdmin } = admin;
+
+    const token = jwt.sign(
+        { id: admin.id, email: admin.email },
+        process.env.SECRET_KEY_JWT,
+        { expiresIn: "1d" }
+    );
+
+    return { admin: safeAdmin, token }
+}
