0f50b4746168bcc718557a36f8153cbda74ad574
novalCandra <dinatanovel5@gmail.com>
2026-05-04 16:31:26 +0700

    feat : ✅ perbaikan prisma utils

--- lib/Prisma.ts
+++ lib/Prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "../generated/prisma/index.js";
 
-const globalForPrisma = global as any
+const globalForPrisma = global as any;
 
 const prisma = globalForPrisma.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== 'production') {
