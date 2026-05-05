5eba70bb8dfb1e8efbf69e10819ee10bb85b4530
novalCandra <dinatanovel5@gmail.com>
2026-05-05 12:46:19 +0700

    feat : ✅ done cors

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ import AuthRouter from './router/AuthRouter.js';
 import PortofolioRouter from './router/PortofolioRouter.js';
 import BeritaRouter from './router/BeritaRouter.js';
 import LayananRouter from './router/layananRouter.js';
-
+import cors from "cors"
 const app = express();
 const port = 2000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 app.use("/public", express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
